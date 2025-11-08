c9d8218470a72c6fc74a198c605d94e6c3c00073
Bill Collins <bill@bill.works>
2025-11-08 10:38:42 +0000

    Modernise react harness

--- libraries/react/src/components.jsx
+++ libraries/react/src/components.jsx
@@ -15,201 +15,141 @@
  * limitations under the License.
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import '#ce-without-children';
 import '#ce-with-children';
 import '#ce-with-properties';
 import '#ce-with-event';
 
-export class ComponentWithoutChildren extends Component {
-  render() {
-    return (
-      <div>
-        <ce-without-children id="ce-without-children"></ce-without-children>
-      </div>
-    );
-  }
+export const ComponentWithoutChildren = () => (
+  <div>
+    <ce-without-children id="ce-without-children"></ce-without-children>
+  </div>
+);
+
+
+export const ComponentWithChildren = () => (
+  <div>
+    <ce-with-children id="ce-with-children"></ce-with-children>
+  </div>
+);
+
+export const ComponentWithChildrenRerender = () => {
+  const [count, setCount] = React.useState(1);
+
+  React.useEffect(() => {
+    Promise.resolve().then(() => setCount(count => count + 1));
+  }, []);
+
+  return (
+    <div>
+      <ce-with-children id="ce-with-children-renderer">{count}</ce-with-children>
+    </div>
+  );
 }
 
-export class ComponentWithChildren extends Component {
-  render() {
-    return (
-      <div>
-        <ce-with-children id="ce-with-children"></ce-with-children>
-      </div>
-    );
-  }
+export const ComponentWithDifferentViews = () => {
+  const [showWC, setShowWC] = React.useState(true);
+  return (
+    <div id="ce-with-different-views">
+      <button onClick={() => setShowWC(showWC => !showWC)}>Toggle views</button>
+      {showWC ? (
+        <ce-with-children></ce-with-children>
+      ) : (
+        <div>Dummy view</div>
+      )}
+    </div>
+  );
 }
 
-export class ComponentWithChildrenRerender extends Component {
-  constructor () {
-    super();
-    this.state = { count: 1 };
-  }
-  componentDidMount () {
-    Promise.resolve().then(_ => this.setState({count: this.state.count += 1}));
-  }
-  componentWillUnmount () {
-    clearInterval(this.interval);
-  }
-  render () {
-    const { count } = this.state;
-    return (
-      <div>
-        <ce-with-children id="ce-with-children-renderer">{count}</ce-with-children>
-      </div>
-    );
-  }
+export const ComponentWithProperties = () => {
+  const data = {
+    bool: true,
+    num: 42,
+    str: 'React',
+    arr: ['R', 'e', 'a', 'c', 't'],
+    obj: {org: 'facebook', repo: 'react'},
+    camelCaseObj: {label: "passed"}
+  };
+  return (
+    <div>
+      <ce-with-properties
+        id="ce-with-properties"
+        bool={data.bool}
+        num={data.num}
+        str={data.str}
+        arr={data.arr}
+        obj={data.obj}
+        camelCaseObj={data.camelCaseObj}
+      ></ce-with-properties>
+    </div>
+  );
 }
 
-export class ComponentWithDifferentViews extends Component {
-  constructor () {
-    super();
-    this.state = { showWC: true };
-  }
-  toggle() {
-    this.setState({ showWC: !this.state.showWC });
-  }
-  render () {
-    const { showWC } = this.state;
-    return (
-      <div id="ce-with-different-views">
-        <button onClick={this.toggle.bind(this)}>Toggle views</button>
-        {showWC ? (
-          <ce-with-children></ce-with-children>
-        ) : (
-          <div>Dummy view</div>
-        )}
-      </div>
-    );
-  }
-}
-
-export class ComponentWithProperties extends Component {
-  render () {
-    const data = {
-      bool: true,
-      num: 42,
-      str: 'React',
-      arr: ['R', 'e', 'a', 'c', 't'],
-      obj: { org: 'facebook', repo: 'react' },
-      camelCaseObj: { label: "passed" }
-    };
-    return (
-      <div>
-        <ce-with-properties id="ce-with-properties"
-          bool={data.bool}
-          num={data.num}
-          str={data.str}
-          arr={data.arr}
-          obj={data.obj}
-          camelCaseObj={data.camelCaseObj}
-        ></ce-with-properties>
-      </div>
-    );
-  }
-}
-
-export class ComponentWithUnregistered extends Component {
-  render () {
-    const data = {
-      bool: true,
-      num: 42,
-      str: 'React',
-      arr: ['R', 'e', 'a', 'c', 't'],
-      obj: { org: 'facebook', repo: 'react' }
-    };
-    return (
-      <div>
-        {/* This element doesn't actually exist.
+export const ComponentWithUnregistered = () => {
+  const data = {
+    bool: true,
+    num: 42,
+    str: 'React',
+    arr: ['R', 'e', 'a', 'c', 't'],
+    obj: {org: 'facebook', repo: 'react'}
+  };
+  return (
+    <div>
+      {/* This element doesn't actually exist.
         It's used to test unupgraded behavior. */}
-        <ce-unregistered id="ce-with-unregistered"
-          bool={data.bool}
-          num={data.num}
-          str={data.str}
-          arr={data.arr}
-          obj={data.obj}
-        ></ce-unregistered>
-      </div>
-    );
-  }
+      <ce-unregistered
+        id="ce-with-unregistered"
+        bool={data.bool}
+        num={data.num}
+        str={data.str}
+        arr={data.arr}
+        obj={data.obj}
+      ></ce-unregistered>
+    </div>
+  );
 }
 
-export class ComponentWithImperativeEvent extends Component {
-  constructor() {
-    super();
-    this.state = {
-      eventHandled: false
-    };
-    this.handleTestEvent = this.handleTestEvent.bind(this);
-  }
-  componentDidMount() {
-    this.wc.addEventListener('camelEvent', this.handleTestEvent);
-  }
-  handleTestEvent(e) {
-    this.setState({ eventHandled: true });
-  }
-  render() {
-    let state = this.state;
-    return (
-      <div>
-        <div id="ce-with-imperative-event-handled" ref={(el) => this.handled = el}>{state.eventHandled.toString()}</div>
-        <ce-with-event id="ce-with-imperative-event" ref={(el) => this.wc = el}>
-          Imperative
-        </ce-with-event>
-      </div>
-    );
-  }
+export const ComponentWithImperativeEvent = () => {
+  const [eventHandled, setEventHandled] = React.useState(false);
+  const ref = React.useRef();
+
+  React.useEffect(() => {
+    ref.current?.addEventListener('camelEvent', () => setEventHandled(true));
+  }, [])
+
+  return (
+    <div>
+      <div id="ce-with-imperative-event-handled">{eventHandled.toString()}</div>
+      <ce-with-event id="ce-with-imperative-event" ref={ref}>
+        Imperative
+      </ce-with-event>
+    </div>
+  );
 }
 
-export class ComponentWithDeclarativeEvent extends Component {
-  constructor() {
-    super();
-    this.state = {
-      lowercaseHandled: false,
-      kebabHandled: false,
-      camelHandled: false,
-      capsHandled: false,
-      pascalHandled: false
-    };
-    this.handleLowercaseEvent = this.handleLowercaseEvent.bind(this);
-    this.handleKebabEvent = this.handleKebabEvent.bind(this);
-    this.handleCamelEvent = this.handleCamelEvent.bind(this);
-    this.handleCapsEvent = this.handleCapsEvent.bind(this);
-    this.handlePascalEvent = this.handlePascalEvent.bind(this);
-  }
-  handleLowercaseEvent(e) {
-    this.setState({ lowercaseHandled: true });
-  }
-  handleKebabEvent(e) {
-    this.setState({ kebabHandled: true });
-  }
-  handleCamelEvent(e) {
-    this.setState({ camelHandled: true });
-  }
-  handleCapsEvent(e) {
-    this.setState({ capsHandled: true });
-  }
-  handlePascalEvent(e) {
-    this.setState({ pascalHandled: true });
-  }
-  render() {
-    let state = this.state;
-    return (
-      <div>
-        <div ref={(el) => this.lowercase = el}>lowercase: {state.lowercaseHandled.toString()}</div>
-        <div ref={(el) => this.kebab = el}>kebab-case: {state.kebabHandled.toString()}</div>
-        <div ref={(el) => this.camel = el}>camelCase: {state.camelHandled.toString()}</div>
-        <div ref={(el) => this.caps = el}>CAPScase: {state.capsHandled.toString()}</div>
-        <div ref={(el) => this.pascal = el}>PascalCase: {state.pascalHandled.toString()}</div>
-        <ce-with-event id="ce-with-declarative-event"
-          onlowercaseevent={this.handleLowercaseEvent}
-          onkebab-event={this.handleKebabEvent}
-          oncamelEvent={this.handleCamelEvent}
-          onCAPSevent={this.handleCapsEvent}
-          onPascalEvent={this.handlePascalEvent}
-        >Declarative</ce-with-event>
-      </div>
-    );
-  }
+export const ComponentWithDeclarativeEvent = () => {
+  const [lowercaseHandled, setLowercaseHandled] = React.useState(false);
+  const [kebabHandled, setKebabHandled] = React.useState(false);
+  const [camelHandled, setCamelHandled] = React.useState(false);
+  const [capsHandled, setCapsHandled] = React.useState(false);
+  const [pascalHandled, setPascalHandled] = React.useState(false);
+  return (
+    <div>
+      <div>lowercase: {lowercaseHandled.toString()}</div>
+      <div>kebab-case: {kebabHandled.toString()}</div>
+      <div>camelCase: {camelHandled.toString()}</div>
+      <div>CAPScase: {capsHandled.toString()}</div>
+      <div>PascalCase: {pascalHandled.toString()}</div>
+      <ce-with-event
+        id="ce-with-declarative-event"
+        onlowercaseevent={() => setLowercaseHandled(true)}
+        onkebab-event={() => setKebabHandled(true)}
+        oncamelEvent={() => setCamelHandled(true)}
+        onCAPSevent={() => setCapsHandled(true)}
+        onPascalEvent={() => setPascalHandled(true)}
+      >Declarative
+      </ce-with-event>
+    </div>
+  );
 }
